7197434989fa23db1622df02c2758808f44ba5e8
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-01-25 15:53:51 +0530

    made some changes in app folder

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import {Poppins} from "next/font/google";
+
+import { cn } from '@/lib/utils';
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ["400","600"]
+});
+
+export const Logo = ()=> {
+  return(
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image
+        src="/logo.svg"
+        alt="Logo"
+        width="40"
+        height="40"
+      />
+    </div>
+  )
+}
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import  React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -32,7 +32,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -51,7 +51,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       />
     )
   }
-)
+);
 Button.displayName = "Button"
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
