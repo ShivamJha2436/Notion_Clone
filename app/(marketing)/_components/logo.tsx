2d07555e6a16378970db1fb21f6a9f6deacfa546
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-01-28 22:51:30 +0530

    Changes in the hero section

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -1,22 +1,25 @@
-import Image from 'next/image';
-import {Poppins} from "next/font/google";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-import { cn } from '@/lib/utils';
 
-const font = Poppins({
-  subsets: ['latin'],
-  weight: ["400","600"]
+const font = Poppins ({
+  subsets: ["latin"],
+  weight: ["400", "600"]
 });
 
-export const Logo = ()=> {
-  return(
+export const Logo = () => {
+  return (
     <div className="hidden md:flex items-center gap-x-2">
       <Image
-        src="/logo.svg"
-        alt="Logo"
+        src="/logo.png"
+        alt="logo"
         width="40"
         height="40"
       />
+      <p className={cn("font-semibold", font.className)}>
+        Notion
+      </p>
     </div>
-  )
+  );
 }
