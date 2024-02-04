5bc657a2c513672b07a255a5c5e523c6f5e17908
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-02-04 22:02:42 +0530

    Added UserButton

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,13 +1,15 @@
 "use client"
 
 import { useConvexAuth } from "convex/react";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import { Link } from "lucide-react";
 
 
 export const Navbar = () => {
@@ -21,7 +23,7 @@ export const Navbar = () => {
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
         {isLoading && (
-          <p>Loading...</p>
+          <Spinner/>
         )}
         {!isAuthenticated && !isLoading &&(
           <>
@@ -35,6 +37,16 @@ export const Navbar = () => {
               Get Notion Free
             </Button>
           </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+          <Button variant="ghost" size="sm" asChild>
+            <Link href="/documents">
+              Enter Notion
+            </Link>
+          </Button>
+          <UserButton/>
           </>
         )}
         <ModeToggle/>
