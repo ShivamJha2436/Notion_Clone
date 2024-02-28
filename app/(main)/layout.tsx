76130c8bd286273de6bd288d36727426f511ff2b
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-02-28 17:36:14 +0530

    Enhanced Sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@
 import { useConvexAuth } from "convex/react";
 import { Spinner } from "@/components/spinner";
 import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({
   children
@@ -18,13 +19,16 @@ const MainLayout = ({
       </div>
     );
   }
-  if (!isAuthenticated){
+  if (!isAuthenticated) {
     return redirect("/");
-  }  
+  }
 
   return (
-    <div className="h-full flex dark:bg-[]">
-      {children}
+    <div className="h-full flex dark:bg-[#000000]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">
+        {children}
+      </main>
     </div>
   );
 }
