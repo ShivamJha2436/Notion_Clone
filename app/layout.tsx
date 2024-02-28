09098dec3f2af30c5d868a64f2e615566ed0da80
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-02-28 09:46:33 +0530

    fixed routing error

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider 
+          <ThemeProvider
             attribute='class'
             defaultTheme='system'
             enableSystem
