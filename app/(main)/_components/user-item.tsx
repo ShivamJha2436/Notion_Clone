017d4a4f6ade41fff47f16a1c7cfb532e2c90819
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-03-10 23:59:04 +0530

    User Item

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -1,9 +1,36 @@
 "use client";
 
+import { ChevronsLeftRight } from "lucide-react";
+import { useUser } from "@clerk/clerk-react";
+
+import {
+  Avatar,
+  AvatarImage
+} from "@/components/ui/avatar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+
 export const UserItem = () => {
+
+  const { user } = useUser();
+
   return (
-    <div>
-      User Item
-    </div>
+   <DropdownMenu>
+    <DropdownMenuTrigger asChild>
+      <div role="button" className="flex items-center text-sm p-3 w-full hover:bg-primary/5">
+        <div className="gap-x-2 flex items-center max-w-[150px]">
+
+        </div>
+      </div>
+    </DropdownMenuTrigger>
+   </DropdownMenu>
   )
 }
