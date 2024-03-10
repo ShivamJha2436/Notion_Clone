75e29721e6bc708aaf4e2b69bc57a2e8d2f5bfc2
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-03-10 23:11:11 +0530

    Fixed some typos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-// import { Toaster } from "sonner";
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
-// import { ModalProvider } from "@/components/providers/modal-provider";
-// import { EdgeStoreProvider } from "@/lib/edgestore";
 
 import './globals.css'
 
