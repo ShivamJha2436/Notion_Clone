8ceb1f657ba758883f2068c3e943502fa0d71631
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-03-10 23:07:46 +0530

    Added useEffect hook

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -24,6 +24,12 @@ export const Navigation = () => {
     }
   },[isMobile]);
 
+  useEffect(() => {
+    if(isMobile){
+      collapse();
+    }
+  }, [pathname, isMobile]);
+
   
 
   // handle resize
