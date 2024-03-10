77e4a211b6de4d0f7d95f6d0a95ff47e9085e2f6
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-03-10 23:32:50 +0530

    User Settings

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { PlusCircle } from "lucide-react";
+
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+
 const DocumentsPage = () => {
+
+  const { user } = useUser();
+
   return ( 
-    <div>
-      This is a protected page
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty state image"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty state image"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Notion
+      </h2>
+      <Button>
+        <PlusCircle/>
+      </Button>
     </div>
    );
 }
